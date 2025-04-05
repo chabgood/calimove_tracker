d06e621b6800e0ec9b8fc279add07d621f0d123f
Chris Habgood <chabgood@gmail.com>
2025-04-05 18:10:22 -0500

    day expanmd/collapse works, needs more styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,7 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import '@nathanvda/cocoon'
-import "choices.js/public/assets/styles/choices.css";
+import Choices from "choices.js/public/assets/styles/choices.css";
 document.addEventListener("DOMContentLoaded", function() {
   const selectElements = document.querySelectorAll('select');
   selectElements.forEach(select => {
