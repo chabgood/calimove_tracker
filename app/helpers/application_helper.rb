b939753b67a41160ac0a763959e634a52563e63f
Chris Habgood <chabgood@gmail.com>
2025-04-06 19:32:50 -0500

    cache 4 times

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,30 @@ module ApplicationHelper
 
   def render_flash
     return if @_flash_rendered
-
     render partial: "shared/flash"
+  end
+
+  def exercise_status
+    Rails.cache.fetch("exercise_status", expires_in: 1.week) do
+      ExerciseStatus.order(:name).pluck(:name, :id)
+    end
+  end
+
+  def rest_time
+    Rails.cache.fetch("rest_times", expires_in: 1.week) do
+      RestTime.order(:name).pluck(:name, :id)
+    end
+  end
+
+  def workout_name
+    Rails.cache.fetch("workout_name", expires_in: 1.week) do
+      WorkoutName.order(:name).pluck(:name, :id)
+    end
+  end
+
+  def day_name
+    Rails.cache.fetch("workout_name", expires_in: 1.week) do
+      DayName.order(:number).pluck(:name, :id)
+    end
   end
 end
