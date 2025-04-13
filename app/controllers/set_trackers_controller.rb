c025b2978ab419748a5eda0e4a81facdc589b106
Chris Habgood <chabgood@proton.me>
2025-04-13 10:44:26 -0500

    set trackers form partial, now when you click it will only repalace it not the whole form

--- app/controllers/set_trackers_controller.rb
+++ app/controllers/set_trackers_controller.rb
@@ -1,13 +1,10 @@
 class SetTrackersController < ApplicationController
   def update
-    tracker = SetTracker.find_by(id: params[:id])
+    tracker = SetTracker.includes(:exercise).find_by(id: params[:id])
     tracker.update(permitted_params)
-    # respond_to do |format|
-    # hotflash.flash_message = :some_other_way_to_show_the_message
-    # format.html { render turbo_stream: turbo_stream.replace("your-element-id", partial: "your_partial", locals: { data: @data }) }
-    # render layout: false
-    # render nothing: true
-    # end
+    render turbo_stream: turbo_stream.replace("set_trackers_exercise_#{tracker.id}", 
+                                                partial: "days/set_tracker_form",
+                                                locals: { exercise: tracker.exercise })
   end
 
   private
