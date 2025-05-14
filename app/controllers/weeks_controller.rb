7cb8cbb12142de9c82d97e5accf6430543a91b1a
Chris Habgood <chabgood@proton.me>
2025-05-14 17:42:01 -0500

    bundle

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -9,7 +9,7 @@ class WeeksController < ApplicationController
 
   def duplicate
     week = Week.find_by(id: params[:id])
-    new_week = week.deep_clone(include: {days: :exercises})
+    new_week = week.deep_clone(include: {days: :exercises}, except: [:set_trackers])
     new_week.save!
     redirect_to schedule_path(new_week.schedule)
   end
